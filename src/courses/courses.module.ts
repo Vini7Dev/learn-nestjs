5c36ee1f6cb9b1368769b8f65432de66fc70ff3b
Vinícius Gabriel <viniciusgabriel.dev@gmail.com>
2022-03-25 10:59:39 -0300

    add courses repository to courses provider

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CoursesController } from './courses.controller';
 import { CoursesService } from './courses.service';
+import { CoursesRepository } from './repositories/courses-repository';
 
 @Module({
   controllers: [CoursesController],
-  providers: [CoursesService],
+  providers: [CoursesService, CoursesRepository],
 })
 export class CoursesModule { }
